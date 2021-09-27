5244d0a4eb0ed490bb7e4e07d02f073eb1175f25
euncm <silver_1102@naver.com>
2021-09-27 10:29:09 +0900

    [오류 다수!] 키 해시를 받아 카카오 개발자 페이지 '내 어플리케이션'에 테스트용으로 등록했습니다 로그인 api 이용 구현 중

--- app/src/main/java/com/cookandroid/graduation_project/MainActivity.java
+++ app/src/main/java/com/cookandroid/graduation_project/MainActivity.java
@@ -2,7 +2,19 @@ package com.cookandroid.graduation_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import com.kakao.auth.ApprovalType;
+import com.kakao.auth.AuthType;
+import com.kakao.auth.IApplicationConfig;
+import com.kakao.auth.ISessionConfig;
+import com.kakao.auth.KakaoAdapter;
+import com.kakao.auth.KakaoSDK;
+import java.security.MessageDigest;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +22,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getAppKeyHash();
+    }
+
+    private void getAppKeyHash() {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md;
+                md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String something = new String(Base64.encode(md.digest(), 0));
+                Log.e("Hash key", something);
+            }
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            Log.e("name not found", e.toString());
+        }
     }
 }
