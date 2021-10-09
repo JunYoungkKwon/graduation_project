c39ad54ff4647deec912bfcde76686852882af36
gnoy21 <pmy0237@naver.com>
2021-10-09 15:00:24 +0900

    feat: 승재님이 만드신 home, covid information, report 화면 추가

---
+++ app/src/main/java/com/cookandroid/graduation_project/ReportListActivity.java
@@ -0,0 +1,62 @@
+package com.example.maskreport;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class ReportListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_list);
+
+        RecyclerView reportListView = findViewById(R.id.report_list);
+        ReportAdapter adapter = new ReportAdapter();
+        reportListView.setAdapter(adapter);
+
+    }
+
+
+    class ReportAdapter extends RecyclerView.Adapter<ReportAdapter.ViewHolder> {
+
+        @NonNull
+        @Override
+        public ReportAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.listitem_report, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ReportAdapter.ViewHolder holder, int position) {
+            //
+        }
+
+        @Override
+        public int getItemCount() {
+            return 30;
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public TextView timeText;
+            public TextView reportNumText;
+            public TextView locationText;
+            public TextView progressText;
+
+            public ViewHolder(View view) {
+                super(view);
+//                timeText = (TextView) view.findViewById(R.id.);
+//                reportNumText = (TextView) view.findViewById(R.id.);
+//                locationText = (TextView) view.findViewById(R.id.);
+//                progressText = (TextView) view.findViewById(R.id.);
+            }
+        }
+    }
+}
