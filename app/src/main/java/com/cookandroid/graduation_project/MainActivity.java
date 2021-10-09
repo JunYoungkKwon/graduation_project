6c3d63e49df74016d5c089c6f1a99ac50e42a9fc
JUNYOUNG KWON <gypig12@gachon.ac.kr>
2021-10-09 11:55:27 +0900

    first edit

--- app/src/main/java/com/cookandroid/graduation_project/MainActivity.java
+++ app/src/main/java/com/cookandroid/graduation_project/MainActivity.java
@@ -26,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton btn_login, btn_login_out;
     private Button test_btn;
 
+    //first edit
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
