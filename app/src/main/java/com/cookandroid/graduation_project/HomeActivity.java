fe0e4d494b5ae7bd441b132c77b4fe3318fcd3f6
gnoy21 <pmy0237@naver.com>
2021-10-16 15:08:59 +0900

    feat: kakao email, name db에 저장, HomeActivity로 값 넘기기

--- app/src/main/java/com/cookandroid/graduation_project/HomeActivity.java
+++ app/src/main/java/com/cookandroid/graduation_project/HomeActivity.java
@@ -4,14 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private String name, email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Intent intent = getIntent();
+
+        name = intent.getStringExtra("name");
+        email = intent.getStringExtra("email");
+
+        Log.d("minyong", name+","+email);
 
         Button recogBtn = findViewById(R.id.home_mask_recog_btn);
         Button covidBtn = findViewById(R.id.home_covid_info_btn);
